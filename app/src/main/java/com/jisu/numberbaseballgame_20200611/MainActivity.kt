964feaa1134bededca98765057bb89e3c21c3548
JISU LEE <jsnote2013@gmail.com>
2020-06-11 20:02:03 +0900

    써도 되는 숫자는 배열에 대입

--- app/src/main/java/com/jisu/numberbaseballgame_20200611/MainActivity.kt
+++ app/src/main/java/com/jisu/numberbaseballgame_20200611/MainActivity.kt
@@ -52,7 +52,9 @@ class MainActivity : BaseActivity() {
                     }
                  }
 
+//                조건에 맞는 숫자를 뽑으면 대입 => 무한반복 탈출
                 if(isNumberOk) {
+                    computerNumbers.add(randomNum)
                     break
                 }
             }
