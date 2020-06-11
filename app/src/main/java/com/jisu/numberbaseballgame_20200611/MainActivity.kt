17078058387b88ce51fe335869098f35ddda15c6
JISU LEE <jsnote2013@gmail.com>
2020-06-11 19:59:46 +0900

    뽑아낸 랜덤숫자를 기존 문제 숫자와 비교(중복검사 수행)

--- app/src/main/java/com/jisu/numberbaseballgame_20200611/MainActivity.kt
+++ app/src/main/java/com/jisu/numberbaseballgame_20200611/MainActivity.kt
@@ -2,11 +2,12 @@ package com.jisu.numberbaseballgame_20200611
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BaseActivity() {
     
 //  컴퓨터가 낸 문제 숫자 세개를 저장할 ArrayList
-    val computerNumber = ArrayList<Int>()
+    val computerNumbers = ArrayList<Int>()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,19 +31,30 @@ class MainActivity : BaseActivity() {
         for (i in 0..2) {
             while (true) {
 
-//               TODO 1~9의 숫자를 랜덤으로 뽑자.
 //                0*9 <= (Math.random()*9+1).toInt() < 1*9+1
                 val randomNum = (Math.random()*9+1).toInt()
 
+                Log.d("랜덤값", randomNum.toString())
+
                 var isNumberOk = true
-//                 조건에 맞는 숫자를 뽑으면 무한반복 탈출
+//                조건에 맞는 숫자를 뽑으면 무한반복 탈출
+//                1~9는 이미 랜덤의 범위를 제한해서 만족
+//                중복이 아니어야 사용해도 좋은 숫자로 인정해주자
+//                중복검사: 문제 숫자 배열에 있는 값들을 다 꺼내서 지금 만든 랜덤값과 비교
+//                한번이라도 같은 걸 찾았다면 => 중복, 한번도 없어야 중복이 아니다.
 
-//                TODO 뽑을 숫자를 써도 되는지 검사 로직 => isNumberOk는 거짓으로
+                 for(cpuNum in computerNumbers) {
 
-
-                    if(isNumberOk) {
+//                     랜덤값과 문제로 뽑아둔 숫자가 같다면? 중복, 사용하면 안됨
+                    if (cpuNum == randomNum) {
+                        isNumberOk = false
                         break
                     }
+                 }
+
+                if(isNumberOk) {
+                    break
+                }
             }
         }
     }
